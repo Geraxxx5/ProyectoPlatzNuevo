402156e92645c1cc600157bd254aa83e8dc8f82c
paulabaal12 <106341373+paulabaal12@users.noreply.github.com>
2023-11-12 15:17:04 -0600

    Update Registrodegastos.kt

--- app/src/main/java/com/example/proyectoplatzapplication/ui/registro/Registrodegastos.kt
+++ app/src/main/java/com/example/proyectoplatzapplication/ui/registro/Registrodegastos.kt
@@ -16,9 +16,13 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import android.app.DatePickerDialog
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
+import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Snackbar
@@ -46,79 +50,76 @@ import java.util.Calendar
 import java.util.Locale
 
 
+
+data class Gasto(
+    val fecha: String,
+    val categoria: String,
+    val descripcion: String,
+    val cantidadGastada: String
+)
+
 class RegistroDeGastos : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ProyectoPlatzApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    CrearPantallaRegistro()
-                }
-            }
+            CrearPantallaRegistro()
         }
     }
 }
 
-@SuppressLint("UnrememberedMutableState")
 @Composable
 fun CrearPantallaRegistro() {
-    val gastos = mutableStateListOf<Gasto>()
-    val fechaSeleccionada = mutableStateOf("")
-    var categoriaSeleccionada = remember { mutableStateOf("Uso Personal") }
-    val descripcion = mutableStateOf("")
-    val cantidadGastada = mutableStateOf("")
+    val gastos = remember { mutableStateListOf<Gasto>() }
+    val fechaSeleccionada = remember { mutableStateOf("") }
+    var categoriaSeleccionada by remember { mutableStateOf("Uso Personal") }
+    val descripcion = remember { mutableStateOf("") }
+    val cantidadGastada = remember { mutableStateOf("") }
     var snackbarVisible by remember { mutableStateOf(false) }
+    var showDialog by remember { mutableStateOf(false) }
 
     Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier.padding(16.dp)
     ) {
         Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.CenterVertically
         ) {
             Text(
                 text = "REGISTRO DE GASTOS",
-                style = MaterialTheme.typography.titleMedium,
-                modifier = Modifier
-                    .padding(end = 16.dp) // Agrega espacio de padding en la parte derecha
-                    .padding(16.dp) // Agrega espacio de padding alrededor del título
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier.weight(1f)
             )
-            Icon(
-                imageVector = Icons.Default.Add,
-                contentDescription = "Agregar gasto",
-                modifier = Modifier
-                    .padding(1.dp)
-
-                    .clickable {
-                        snackbarVisible = true
-                    }
-            )
+            Box(
+                modifier = Modifier.size(48.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                IconButton(
+                    onClick = { showDialog = true }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = "Agregar gasto"
+                    )
+                }
+            }
         }
 
-
-        CustomDatePicker(selectedDate = fechaSeleccionada, modifier = Modifier.padding(16.dp))
+        CustomDatePicker(selectedDate = fechaSeleccionada)
 
         DropdownMenu(
             expanded = false,
             onDismissRequest = { },
-            modifier = Modifier.padding(16.dp)
+            modifier = Modifier.padding(vertical = 16.dp)
         ) {
-            androidx.compose.material.DropdownMenuItem(onClick = { categoriaSeleccionada = mutableStateOf("Comida") }) {
+            DropdownMenuItem(onClick = { categoriaSeleccionada = "Comida" }) {
                 Text("Comida")
             }
-            androidx.compose.material.DropdownMenuItem(onClick = { categoriaSeleccionada = mutableStateOf("Deudas") }) {
+            DropdownMenuItem(onClick = { categoriaSeleccionada = "Deudas" }) {
                 Text("Deudas")
             }
-            androidx.compose.material.DropdownMenuItem(onClick = { categoriaSeleccionada = mutableStateOf("Emergencia") }) {
+            DropdownMenuItem(onClick = { categoriaSeleccionada = "Emergencia" }) {
                 Text("Emergencia")
             }
-            androidx.compose.material.DropdownMenuItem(onClick = { categoriaSeleccionada = mutableStateOf("Uso Personal") }) {
+            DropdownMenuItem(onClick = { categoriaSeleccionada = "Uso Personal" }) {
                 Text("Uso Personal")
             }
         }
@@ -127,37 +128,37 @@ fun CrearPantallaRegistro() {
             value = descripcion.value,
             onValueChange = { descripcion.value = it },
             label = { Text(text = "Descripción/Uso") },
-            modifier = Modifier.padding(16.dp)
+            modifier = Modifier.padding(vertical = 16.dp)
         )
 
         TextField(
             value = cantidadGastada.value,
             onValueChange = { cantidadGastada.value = it },
             label = { Text(text = "Gasto") },
-            modifier = Modifier.padding(16.dp),
-            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number)
+            modifier = Modifier.padding(vertical = 16.dp),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
         )
 
         LazyColumn {
             items(gastos) { gasto ->
-                Row(modifier = Modifier.padding(16.dp)) {
-                    Text(text = gasto.fecha)
-                    Text(text = gasto.categoria)
-                    Text(text = gasto.descripcion)
-                    Text(text = gasto.cantidadGastada)
+                Row(modifier = Modifier.padding(vertical = 8.dp)) {
+                    Text(text = gasto.fecha, modifier = Modifier.weight(1f))
+                    Text(text = gasto.categoria, modifier = Modifier.weight(1f))
+                    Text(text = gasto.descripcion, modifier = Modifier.weight(1f))
+                    Text(text = gasto.cantidadGastada, modifier = Modifier.weight(1f))
                 }
             }
         }
 
         Text(
-            text = "Total gastado: ${calcularTotalGastado(gastos)}",
+            text = "Total gastado Q: ${calcularTotalGastado(gastos)}",
             style = MaterialTheme.typography.titleMedium,
-            modifier = Modifier.padding(16.dp)
+            modifier = Modifier.padding(vertical = 16.dp)
         )
 
         if (snackbarVisible) {
             Snackbar(
-                modifier = Modifier.padding(16.dp)
+                modifier = Modifier.padding(vertical = 16.dp)
             ) {
                 Text("Gasto agregado correctamente")
             }
@@ -167,33 +168,33 @@ fun CrearPantallaRegistro() {
             }
         }
     }
-}
 
-data class Gasto(
-    val fecha: String,
-    val categoria: String,
-    val descripcion: String,
-    val cantidadGastada: String)
-
-fun calcularTotalGastado(gastos: List<Gasto>): String {
-    var total = 0.0
-
-    for (gasto in gastos) {
-        total += gasto.cantidadGastada
+    if (showDialog) {
+        AgregarGastoDialog(
+            fechaSeleccionada = fechaSeleccionada,
+            categoriaSeleccionada = categoriaSeleccionada,
+            descripcion = descripcion,
+            cantidadGastada = cantidadGastada,
+            onDismiss = { showDialog = false },
+            onAgregarGasto = {
+                gastos.add(
+                    Gasto(
+                        fecha = fechaSeleccionada.value,
+                        categoria = categoriaSeleccionada,
+                        descripcion = descripcion.value,
+                        cantidadGastada = cantidadGastada.value
+                    )
+                )
+                snackbarVisible = true
+            }
+        )
     }
-
-    return total.toString()
 }
 
-private operator fun Double.plusAssign(cantidadGastada: String) {
-
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CustomDatePicker(selectedDate: MutableState<String>, modifier: Modifier = Modifier) {
+fun CustomDatePicker(selectedDate: MutableState<String>) {
     val context = LocalContext.current
-    val dateFormatter = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+    val dateFormatter = remember { SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()) }
 
     fun showDatePicker() {
         val calendar = Calendar.getInstance()
@@ -206,8 +207,7 @@ fun CustomDatePicker(selectedDate: MutableState<String>, modifier: Modifier = Mo
             { _, year, month, dayOfMonth ->
                 val selectedCalendar = Calendar.getInstance()
                 selectedCalendar.set(year, month, dayOfMonth)
-                val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-                selectedDate.value = dateFormat.format(selectedCalendar.time)
+                selectedDate.value = dateFormatter.format(selectedCalendar.time)
             },
             year,
             month,
@@ -217,62 +217,94 @@ fun CustomDatePicker(selectedDate: MutableState<String>, modifier: Modifier = Mo
         datePickerDialog.show()
     }
 
-    OutlinedButton(
-        onClick = {
-            showDatePicker()
-        },
-        modifier = modifier
+    Button(
+        onClick = { showDatePicker() },
+        modifier = Modifier.padding(vertical = 16.dp)
     ) {
-        Text(text = "Select Date")
+        Text(text = "Seleccionar fecha")
     }
 }
 
-
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun showDatePicker(selectedDate: MutableState<String>) {
-    val context = LocalContext.current
-    val calendar = Calendar.getInstance()
-    val year = calendar.get(Calendar.YEAR)
-    val month = calendar.get(Calendar.MONTH)
-    val day = calendar.get(Calendar.DAY_OF_MONTH)
-
-    val datePickerDialog = DatePickerDialog(
-        context,
-        { _, year, month, dayOfMonth ->
-            val selectedCalendar = Calendar.getInstance()
-            selectedCalendar.set(year, month, dayOfMonth)
-            val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-            selectedDate.value = dateFormat.format(selectedCalendar.time)
+fun AgregarGastoDialog(
+    fechaSeleccionada: MutableState<String>,
+    categoriaSeleccionada: String,
+    descripcion: MutableState<String>,
+    cantidadGastada: MutableState<String>,
+    onDismiss: () -> Unit,
+    onAgregarGasto: () -> Unit
+) {
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        title = { Text(text = "Agregar gasto") },
+        confirmButton = {
+            Button(
+                onClick = {
+                    onAgregarGasto()
+                    onDismiss()
+                }
+            ) {
+                Text(text = "Agregar")
+            }
         },
-        year,
-        month,
-        day
-    )
+        dismissButton = {
+            Button(
+                onClick = onDismiss
+            ) {
+                Text(text = "Cancelar")
+            }
+        },
+        text = {
+            Column {
+                CustomDatePicker(selectedDate = fechaSeleccionada)
 
-    datePickerDialog.show()
+                DropdownMenu(
+                    expanded = false,
+                    onDismissRequest = { },
+                    modifier = Modifier.padding(vertical = 16.dp)
+                ) {
+                    DropdownMenuItem(onClick = { /* Handle category selection */ }) {
+                        Text("Comida")
+                    }
+                    DropdownMenuItem(onClick = { /* Handle category selection */ }) {
+                        Text("Deudas")
+                    }
+                    DropdownMenuItem(onClick = { /* Handle category selection */ }) {
+                        Text("Emergencia")
+                    }
+                    DropdownMenuItem(onClick = { /* Handle category selection */ }) {
+                        Text("Uso Personal")
+                    }
+                }
+
+                TextField(
+                    value = descripcion.value,
+                    onValueChange = { descripcion.value = it },
+                    label = { Text(text = "Descripción/Uso") },
+                    modifier = Modifier.padding(vertical = 16.dp)
+                )
+
+                TextField(
+                    value = cantidadGastada.value,
+                    onValueChange = { cantidadGastada.value = it },
+                    label = { Text(text = "Gasto") },
+                    modifier = Modifier.padding(vertical = 16.dp),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                )
+            }
+        }
+    )
 }
 
+fun calcularTotalGastado(gastos: List<Gasto>): Double {
+    var total = 0.0
 
-/*@Composable
-fun CustomSnackbar(
-    modifier: Modifier = Modifier,
-    content: @Composable (Snackbar.SnackbarLayout) -> Unit
-) {
-    val host = rememberSnackbarHostState()
-
-    LaunchedEffect(host) {
-        host.showSnackbar(
-            message = "",
-            actionLabel = "Dismiss"
-        )
+    for (gasto in gastos) {
+        total += gasto.cantidadGastada.toDoubleOrNull() ?: 0.0
     }
 
-    SnackbarHost(
-        hostState = host,
-        snackbar = content
-    )
-}*/
+    return total
+}
 
 @Preview
 @Composable
